64539886317660dd71011b9efda14b8a1b4dd35f
Harshad Nawathe <harshad.nawathe@gmail.com>
2024-03-21 19:59:45 +0530

    Add recursive solution for reverse-linked-list

--- reverse-linked-list/solution.go
+++ reverse-linked-list/solution.go
@@ -1,5 +1,6 @@
 package reverselinkedlist
 
+// iterative solution
 // time: O(n)
 // space: O(1)
 func reverseList(head *ListNode) *ListNode {
@@ -13,3 +14,23 @@ func reverseList(head *ListNode) *ListNode {
 	}
 	return prev
 }
+
+// // recursive solution
+// // // time: O(n)
+// // // space: O(n) -- call stack
+// func reverseList(head *ListNode) *ListNode {
+// 	if head == nil {
+// 		return nil
+// 	}
+//
+// 	if head.Next == nil {
+// 		return head
+// 	}
+//
+// 	tail := head.Next
+// 	newHead := reverseList(tail)
+// 	tail.Next = head
+// 	head.Next = nil
+//
+// 	return newHead
+// }
