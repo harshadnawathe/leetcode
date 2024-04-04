b71908fb4529bc7d91164ff2be6927db92e29bcd
Harshad Nawathe <harshad.nawathe@gmail.com>
2024-04-04 07:59:03 +0530

    Add dynamic programming based solution for combination-sum

--- combination-sum/solution.go
+++ combination-sum/solution.go
@@ -1,6 +1,32 @@
 package combinationsum
 
 func combinationSum(candidates []int, target int) [][]int {
+	combinations := make([][][]int, target+1)
+	for i := range combinations {
+		combinations[i] = make([][]int, 0)
+	}
+	combinations[0] = append(combinations[0], []int{})
+
+	for _, candidate := range candidates {
+		for sum := 1; sum < len(combinations); sum++ {
+			if sum-candidate < 0 {
+				continue
+			}
+
+			for _, combination := range combinations[sum-candidate] {
+				var newCombination []int
+				newCombination = append(newCombination, combination...)
+				newCombination = append(newCombination, candidate)
+
+				combinations[sum] = append(combinations[sum], newCombination)
+			}
+		}
+	}
+
+	return combinations[target]
+}
+
+func combinationSum1(candidates []int, target int) [][]int {
 	result := [][]int{}
 
 	var f func(int, int, []int)
