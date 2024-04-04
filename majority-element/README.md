ed803e8adab529e6ac8c905e94182cdf2e5654c4
Harshad Nawathe <harshad.nawathe@gmail.com>
2024-04-04 15:59:33 +0530

    Add solution for majority-element

---
+++ majority-element/README.md
@@ -0,0 +1,3 @@
+# 169. Majority Element
+
+Click [here](https://leetcode.com/problems/majority-element/) for the leetcode problem statement.
---
+++ majority-element/solution.go
@@ -0,0 +1,19 @@
+package majorityelement
+
+func majorityElement(nums []int) int {
+	candidate, count := 0, 0
+
+	for _, num := range nums {
+		if count == 0 {
+			candidate = num
+		}
+
+		if candidate == num {
+			count++
+		} else {
+			count--
+		}
+	}
+
+	return candidate
+}
