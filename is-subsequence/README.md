fd2457dac85e293fe1c0b3f6fd797963cbfacd70
Harshad Nawathe <harshad.nawathe@gmail.com>
2024-04-24 09:14:37 +0530

    Add solution for is-subsequence

---
+++ is-subsequence/README.md
@@ -0,0 +1,3 @@
+# 392. Is Subsequence
+
+Click [here](https://leetcode.com/problems/is-subsequence/) for the leetcode problem statement.
---
+++ is-subsequence/solution.go
@@ -0,0 +1,11 @@
+package issubsequence
+
+func isSubsequence(s string, t string) bool {
+	i := 0
+	for j := 0; j < len(t) && i < len(s); j++ {
+		if s[i] == t[j] {
+			i++
+		}
+	}
+	return i == len(s)
+}
