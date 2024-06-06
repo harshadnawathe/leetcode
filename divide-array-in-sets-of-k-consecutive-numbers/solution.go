829344b9ecd54eb857c6d84b019c9783599084e4
Harshad Nawathe <harshad.nawathe@gmail.com>
2024-06-06 14:55:33 +0530

    Add optimised solution for divide-array-in-sets-of-k-consecutive-numbers

--- divide-array-in-sets-of-k-consecutive-numbers/solution.go
+++ divide-array-in-sets-of-k-consecutive-numbers/solution.go
@@ -1,52 +1,37 @@
 package dividearrayinsetsofkconsecutivenumbers
 
-import "github.com/emirpasic/gods/maps/treemap"
+import "sort"
 
 func isPossibleDivide(nums []int, k int) bool {
 	if len(nums)%k != 0 {
 		return false
 	}
 
-	m := treemap.NewWithIntComparator()
+	sort.Ints(nums)
 
+	numCount := map[int]int{}
 	for _, num := range nums {
-		addNum(m, num)
+		numCount[num]++
 	}
 
-	for m.Size() > 0 {
-		it := m.Iterator()
-		it.Next()
-		num := it.Key().(int)
+	numGroups := 0
 
-		removeNum(m, num)
+	for _, num := range nums {
+		if numCount[num] == 0 {
+			continue
+		}
+
+		numGroups++
+		numCount[num]--
 
 		for i := 1; i < k; i++ {
-			if _, ok := m.Get(num + i); !ok {
+			if numCount[num+i] == 0 {
 				return false
 			}
 
-			removeNum(m, num+i)
+			numCount[num+i]--
 		}
 	}
 
-	return true
-}
-
-func addNum(m *treemap.Map, num int) {
-	if cnt, ok := m.Get(num); ok {
-		m.Put(num, cnt.(int)+1)
-	} else {
-		m.Put(num, 1)
-	}
-}
-
-func removeNum(m *treemap.Map, num int) {
-	if val, ok := m.Get(num); ok {
-		cnt := val.(int)
-		if cnt > 1 {
-			m.Put(num, cnt-1)
-		} else {
-			m.Remove(num)
-		}
-	}
+	return numGroups == len(nums)/k
 }
