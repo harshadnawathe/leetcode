a3c06adcf3dd44070a648fd7a38b22885fe4becc
Harshad Nawathe <harshad.nawathe@gmail.com>
2024-06-06 14:47:50 +0530

    Add optimised solution for hand-of-straights

--- hand-of-straights/solution.go
+++ hand-of-straights/solution.go
@@ -1,106 +1,37 @@
 package handofstraights
 
-import "container/heap"
+import "sort"
 
 func isNStraightHand(hand []int, groupSize int) bool {
 	if len(hand)%groupSize != 0 {
 		return false
 	}
 
-	set := newNumSet()
+	sort.Ints(hand)
+
+	numCount := make(map[int]int)
+	for _, num := range hand {
+		numCount[num]++
+	}
+
+	numGroups := 0
 
 	for _, num := range hand {
-		add(set, num)
-	}
+		if numCount[num] == 0 {
+			continue
+		}
 
-	for set.Len() > 0 {
-		num := minimum(set)
-
-		remove(set, num)
-
+		numCount[num]--
 		for i := 1; i < groupSize; i++ {
-			if !contains(set, num+i) {
+			if numCount[num+i] == 0 {
 				return false
 			}
-			remove(set, num+i)
+
+			numCount[num+i]--
 		}
+
+		numGroups++
 	}
 
-	return true
-}
-
-type CountPos struct {
-	Count, Pos int
-}
-
-type numSet struct {
-	numCountPos map[int]*CountPos
-	minHeap     []int
-}
-
-func newNumSet() *numSet {
-	return &numSet{
-		numCountPos: make(map[int]*CountPos),
-		minHeap:     []int{},
-	}
-}
-
-func add(ns *numSet, num int) {
-	if cp, ok := ns.numCountPos[num]; ok {
-		cp.Count += 1
-	} else {
-		heap.Push(ns, num)
-	}
-}
-
-func remove(ns *numSet, num int) {
-	if cp, ok := ns.numCountPos[num]; ok {
-		if cp.Count > 1 {
-			cp.Count -= 1
-		} else {
-			heap.Remove(ns, cp.Pos)
-		}
-	}
-}
-
-func contains(ns *numSet, num int) bool {
-	_, ok := ns.numCountPos[num]
-	return ok
-}
-
-func minimum(ns *numSet) int {
-	return ns.minHeap[0]
-}
-
-func (ns *numSet) Len() int {
-	return len(ns.minHeap)
-}
-
-func (ns *numSet) Less(i int, j int) bool {
-	return ns.minHeap[i] < ns.minHeap[j]
-}
-
-func (ns *numSet) Swap(i int, j int) {
-	ns.minHeap[i], ns.minHeap[j] = ns.minHeap[j], ns.minHeap[i]
-	ns.numCountPos[ns.minHeap[j]].Pos = j
-	ns.numCountPos[ns.minHeap[i]].Pos = i
-}
-
-func (ns *numSet) Push(x any) {
-	num := x.(int)
-
-	ns.numCountPos[num] = &CountPos{1, len(ns.minHeap)}
-	ns.minHeap = append(ns.minHeap, num)
-}
-
-func (ns *numSet) Pop() any {
-	old := ns.minHeap
-	n := len(old)
-
-	x := old[n-1]
-
-	ns.minHeap = old[:n-1]
-	delete(ns.numCountPos, x)
-
-	return x
+	return numGroups == len(hand)/groupSize
 }
---
+++ hand-of-straights/solution1.go
@@ -0,0 +1,106 @@
+package handofstraights
+
+import "container/heap"
+
+func isNStraightHand1(hand []int, groupSize int) bool {
+	if len(hand)%groupSize != 0 {
+		return false
+	}
+
+	set := newNumSet()
+
+	for _, num := range hand {
+		add(set, num)
+	}
+
+	for set.Len() > 0 {
+		num := minimum(set)
+
+		remove(set, num)
+
+		for i := 1; i < groupSize; i++ {
+			if !contains(set, num+i) {
+				return false
+			}
+			remove(set, num+i)
+		}
+	}
+
+	return true
+}
+
+type CountPos struct {
+	Count, Pos int
+}
+
+type numSet struct {
+	numCountPos map[int]*CountPos
+	minHeap     []int
+}
+
+func newNumSet() *numSet {
+	return &numSet{
+		numCountPos: make(map[int]*CountPos),
+		minHeap:     []int{},
+	}
+}
+
+func add(ns *numSet, num int) {
+	if cp, ok := ns.numCountPos[num]; ok {
+		cp.Count += 1
+	} else {
+		heap.Push(ns, num)
+	}
+}
+
+func remove(ns *numSet, num int) {
+	if cp, ok := ns.numCountPos[num]; ok {
+		if cp.Count > 1 {
+			cp.Count -= 1
+		} else {
+			heap.Remove(ns, cp.Pos)
+		}
+	}
+}
+
+func contains(ns *numSet, num int) bool {
+	_, ok := ns.numCountPos[num]
+	return ok
+}
+
+func minimum(ns *numSet) int {
+	return ns.minHeap[0]
+}
+
+func (ns *numSet) Len() int {
+	return len(ns.minHeap)
+}
+
+func (ns *numSet) Less(i int, j int) bool {
+	return ns.minHeap[i] < ns.minHeap[j]
+}
+
+func (ns *numSet) Swap(i int, j int) {
+	ns.minHeap[i], ns.minHeap[j] = ns.minHeap[j], ns.minHeap[i]
+	ns.numCountPos[ns.minHeap[j]].Pos = j
+	ns.numCountPos[ns.minHeap[i]].Pos = i
+}
+
+func (ns *numSet) Push(x any) {
+	num := x.(int)
+
+	ns.numCountPos[num] = &CountPos{1, len(ns.minHeap)}
+	ns.minHeap = append(ns.minHeap, num)
+}
+
+func (ns *numSet) Pop() any {
+	old := ns.minHeap
+	n := len(old)
+
+	x := old[n-1]
+
+	ns.minHeap = old[:n-1]
+	delete(ns.numCountPos, x)
+
+	return x
+}
