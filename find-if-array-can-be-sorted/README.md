00708aa6859a3309fcf2b79fc95e9ff7f8c677aa
Harshad Nawathe <harshad.nawathe@gmail.com>
2024-11-06 10:27:22 +0530

    Add solution for find-if-array-can-be-sorted

---
+++ find-if-array-can-be-sorted/README.md
@@ -0,0 +1,3 @@
+# 3011. Find if Array Can Be Sorted
+
+Click [here](https://leetcode.com/problems/find-if-array-can-be-sorted/) for the leetcode problem statement.
---
+++ find-if-array-can-be-sorted/solution_test.go
@@ -0,0 +1,43 @@
+package findifarraycanbesorted
+
+import "testing"
+
+func Test_canSortArray(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "Example 1",
+			args: args{
+				nums: []int{8, 4, 2, 30, 15},
+			},
+			want: true,
+		},
+		{
+			name: "Example 2",
+			args: args{
+				nums: []int{1, 2, 3, 4, 5},
+			},
+			want: true,
+		},
+		{
+			name: "Example 3",
+			args: args{
+				nums: []int{3, 16, 8, 4, 2},
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := canSortArray(tt.args.nums); got != tt.want {
+				t.Errorf("canSortArray() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
