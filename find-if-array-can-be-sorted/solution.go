926537eccf7ac9d38251e532051b92891b1cc33a
Harshad Nawathe <harshad.nawathe@gmail.com>
2024-11-06 10:46:22 +0530

    Refactor solution for find-if-array-can-be-sorted
    - Return early if elements can't be swapped
    - Use library function to count number of bits

--- find-if-array-can-be-sorted/solution.go
+++ find-if-array-can-be-sorted/solution.go
@@ -1,48 +1,29 @@
 package findifarraycanbesorted
 
 import (
-	"cmp"
+	"math/bits"
 )
 
 func canSortArray(nums []int) bool {
 	numBits := map[int]int{}
 	for _, num := range nums {
-		numBits[num] = bitsCount(num)
-	}
-
-	isBitCountSame := func(i, j int) bool {
-		return numBits[nums[i]] == numBits[nums[j]]
+		numBits[num] = bits.OnesCount16(uint16(num))
 	}
 
 	for i := 1; i < len(nums); i++ {
-		for j := i; j > 0 && nums[j-1] > nums[j] && isBitCountSame(j, j-1); j-- {
+		for j := i; j > 0; j-- {
+			if nums[j-1] <= nums[j] {
+				continue
+			}
+
+			if numBits[nums[j]] != numBits[nums[j-1]] {
+				return false
+			}
+
 			nums[j], nums[j-1] = nums[j-1], nums[j]
+
 		}
 	}
 
-	return isSorted(nums)
-}
-
-func bitsCount(n int) int {
-	count := 0
-
-	for i, mask := 0, 1; i <= 8; i++ {
-
-		if mask&n != 0 {
-			count++
-		}
-
-		mask <<= 1
-	}
-
-	return count
-}
-
-func isSorted[T cmp.Ordered](a []T) bool {
-	for i := range len(a) - 1 {
-		if a[i] > a[i+1] {
-			return false
-		}
-	}
 	return true
 }
